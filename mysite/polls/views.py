d37a04926c3bea91f2b740861510fa61e04fce77
rashijais <rashijaiswal2298@gmail.com>
2018-09-14 16:19:16 +0530

    polls tut-3 choice n results

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Choice, Question
 
 # Create your views here.
 
@@ -16,10 +19,24 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse("You Are Looking At The Results Of Question %s" %question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
 
 def vote(request, question_id):
-    return HttpResponse("You Are Voting At Question %s" %question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
